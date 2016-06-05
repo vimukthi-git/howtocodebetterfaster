1f279ef7c865d05ef2bf48e2b7f3b32a2eecccd6
Vimukthi <vbwickramasinghe@gmail.com>
2016-06-05 17:38:58 +0800

    controller added

--- router.js
+++ router.js
@@ -1,46 +1,13 @@
 'use strict';
 const route = require('koa-route');
+const controller = require('./controller');
 // controller dependancies
 const views = require('co-views');
 const parse = require('co-body');
 
-let messages = [{
-    id: 0,
-    message: 'Koa next generation web framework for node.js'
-}, {
-    id: 1,
-    message: 'Koa is a new web framework designed by the team behind Express'
-}];
-
-const render = views(__dirname + '/views', {
-    map: {
-        html: 'swig'
-    }
-});
-
 module.exports = (app) => {
-  app.use(route.get('/', function* home(ctx) {
-      let msgs = messages.slice();
-      msgs.reverse();
-      this.body = yield render('list', {
-          'messages': msgs
-      });
-  }));
-  app.use(route.get('/messages', function* list() {
-      this.body = yield messages;
-  }));
-  app.use(route.get('/messages/:id', function* fetch(id) {
-      const message = messages[id];
-      if (!message) {
-          this.throw(404, 'message with id = ' + id + ' was not found');
-      }
-      this.body = yield message;
-  }));
-  app.use(route.post('/messages', function* create() {
-      const message = yield parse(this);
-      const id = messages.push(message) - 1;
-      messages.shift();
-      message.id = id;
-      this.redirect('/');
-  }));
+  app.use(route.get('/', controller.home));
+  app.use(route.get('/messages', controller.list));
+  app.use(route.get('/messages/:id', controller.fetch));
+  app.use(route.post('/messages', controller.create));
 }
